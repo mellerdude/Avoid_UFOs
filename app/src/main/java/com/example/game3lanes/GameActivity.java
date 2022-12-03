810836b5c7ebdedaa7cdfbca9069efaf9e91765d
mellerdude <omrimeller@gmail.com>
2022-12-03 20:46:53 +0200

    Added game functionality -UFOs On collision: -Vibration -Toast messages

--- app/src/main/java/com/example/game3lanes/GameActivity.java
+++ app/src/main/java/com/example/game3lanes/GameActivity.java
@@ -2,47 +2,45 @@ package com.example.game3lanes;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.graphics.drawable.shapes.Shape;
+import android.content.Context;
+import android.os.Build;
 import android.os.Bundle;
-import android.os.CountDownTimer;
-import android.util.Log;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
 import android.view.View;
-import android.widget.ArrayAdapter;
+import android.view.ViewTreeObserver;
 import android.widget.LinearLayout;
-import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.material.button.MaterialButton;
 import com.google.android.material.imageview.ShapeableImageView;
 import com.google.android.material.textview.MaterialTextView;
-
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Timer;
 import java.util.TimerTask;
 
 public class GameActivity extends AppCompatActivity {
-    final int DELAY = 1000;
     private MaterialTextView game_LBL_score;
     private MaterialButton game_FAB_left;
     private MaterialButton game_FAB_right;
     private ShapeableImageView[] game_IMG_spaceships;
+    private ShapeableImageView[] game_IMG_UFO;
     private ShapeableImageView[] game_IMG_hearts;
-    private ListView[] laneList;
-    private ShapeableImageView game_IMG_alien;
+    private LinearLayout[] game_LANE_lane;
+    private TextView game_ET_toast;
     private gameManager manager;
-    private LinearLayout ll;
     private long startTime = 0;
     private Timer timer;
 
+
+
+    ///Creating the app, creating a game manager
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
-
-
         findViews();
-
         manager = new gameManager();
 
 
@@ -54,32 +52,170 @@ public class GameActivity extends AppCompatActivity {
         });
 
         startTime();
+        
+        
     }
+
+    ///Getting the length of the layout(Because OnCreate denies it) and creating runnable for the game
 
     private void startTime() {
         startTime = System.currentTimeMillis();
         timer = new Timer();
+        ViewTreeObserver vto = game_LANE_lane[0].getViewTreeObserver();
+        vto.addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                game_LANE_lane[0].getViewTreeObserver().removeOnGlobalLayoutListener(this);
+                int width  = game_LANE_lane[0].getMeasuredWidth();
+                int height = game_LANE_lane[0].getMeasuredHeight();
+                manager.setLaneLength(height);
+                System.out.println("your lane length and width is" + width +"," + height);
+
+            }
+        });
         timer.scheduleAtFixedRate(new TimerTask() {
             @Override
             public void run() {
                 runOnUiThread(() -> updateUI());
             }
-        }, manager.DELAY, manager.DELAY);
-                }
+        }, manager.getDELAY(), manager.getDELAY());
+
+
+    }
+
+    ///Updating the UI each interval
 
     private void updateUI() {
-        manager.setScore(System.currentTimeMillis() - startTime);
-        int seconds = (int) (manager.getScore() / manager.DELAY);
-        //double points
-        if(seconds>manager.BONUS)
-            seconds *= 10;
-        game_LBL_score.setText(String.format("%04d", seconds));
+        if(manager.getLaneLength()>0) {
+            manager.setScore(((System.currentTimeMillis() - startTime)/ manager.getDELAY()) + manager.getBonusScore());
+            game_LBL_score.setText(String.format("%06d", manager.getScore()));
+            manager.createUFO();
+            moveUFO();
+            resetPos();
+        }
     }
+
+    /// move a UFO in each of the lanes if a UFO exists
+
+    private void moveUFO() {
+        for (int i =0 ; i<manager.getLANES();i++) {
+            if (manager.getHasAsteroid(i)) {
+                game_IMG_UFO[i].setVisibility(View.VISIBLE);
+                LinearLayout.LayoutParams params = (LinearLayout.LayoutParams) game_LANE_lane[i].getLayoutParams();
+                params.setMargins(params.leftMargin, manager.get_UFO_locationY(i) + manager.getMovementSpeed(i), params.rightMargin, params.bottomMargin);
+                game_LANE_lane[i].setLayoutParams(params);
+                manager.moveUFO();
+            }
+        }
+    }
+
+    ///If a UFO has reached the end of the lane reset his position. If a collision occured, activate the collision function for that lane.
+
+    private void resetPos() {
+        for (int i =0 ; i<manager.getLANES();i++) {
+            if(manager.gotToEnd(i)) {
+                if(manager.check_collision(i)){
+                    collision(i);
+                }else{
+                    manager.setBonusScore(50);
+                }
+                LinearLayout.LayoutParams params = (LinearLayout.LayoutParams) game_LANE_lane[i].getLayoutParams();
+                params.setMargins(params.leftMargin, 0, params.rightMargin, params.bottomMargin);
+                manager.setUFO_locationY(i, -manager.get_UFO_locationY(i));
+                manager.setHasUFO(i, false);
+            }
+        }
+    }
+
+    /* When a collision occurs:
+    1. Decrease hearts by one
+    2. Vibrate the phone
+    3. Make a toast message
+    4. Set bonus score negative 100
+    5. make the UFO invisible
+     */
+
+    private void collision(int i){
+        decrease_hearts();
+        vibrate();
+        toast();
+        manager.setBonusScore(-100);
+        invisibleUFO(i);
+    }
+
+    ///If a UFO has reached the end of the lane or collided with the spaceship make it invisible.
+
+    private void invisibleUFO(int i){
+        game_IMG_UFO[i].setVisibility(View.INVISIBLE);
+    }
+
+
+
+
+    ///Toast message for collision
+
+    private void toast() {
+        String name = String.valueOf(game_ET_toast.getText());
+        Toast
+                .makeText(this,"OUCH DODGE THE ALIENS PLEASE" + name,Toast.LENGTH_SHORT)
+                .show();
+    }
+
+    ///Vibrate for collision
+
+    private void vibrate() {
+
+        Vibrator v = null;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+            v.vibrate(VibrationEffect.createOneShot(500, VibrationEffect.DEFAULT_AMPLITUDE));
+        }
+    }
+
+    ///Decreasing hearts for collision
+
+    private void decrease_hearts() {
+            if(manager.getLife() != 0) {
+                game_IMG_hearts[manager.getLife() - 1].setVisibility(View.INVISIBLE);
+                manager.setLife(manager.getLife() - 1);
+            }
+            if(manager.getLife() == 0) {
+                manager.setLife(manager.getMAXLIFE());
+                for (int i = 0; i < manager.getMAXLIFE(); i++) {
+                    game_IMG_hearts[i].setVisibility(View.VISIBLE);
+                }
+            }
+
+    }
+
+    ///When clicking fab buttons
 
     private void clicked(int dir) {
         if(dir>0)goRight();
         else goLeft();
     }
+
+    ///If clicking right and the spaceship is not at the far end - go right
+
+    public void goRight() {
+        if(manager.getSpaceshipLocation()!=manager.getMAXRIGHT()) {
+            game_IMG_spaceships[manager.getSpaceshipLocation()].setVisibility(View.INVISIBLE);
+            game_IMG_spaceships[manager.getSpaceshipLocation()+1].setVisibility(View.VISIBLE);
+            manager.setSpaceship_location(1);
+        }
+    }
+
+    ///If clicking left and the spaceship is not at the far end - go left
+
+    public void goLeft() {
+        if(manager.getSpaceshipLocation()!=manager.getMAXLEFT()) {
+            game_IMG_spaceships[manager.getSpaceshipLocation()].setVisibility(View.INVISIBLE);
+            game_IMG_spaceships[manager.getSpaceshipLocation()-1].setVisibility(View.VISIBLE);
+            manager.setSpaceship_location(-1);
+        }
+    }
+
+    ///Set up views
 
     private void findViews() {
         game_LBL_score = findViewById(R.id.game_LBL_score);
@@ -95,24 +231,19 @@ public class GameActivity extends AppCompatActivity {
                 findViewById(R.id.game_IMG_heart2),
                 findViewById(R.id.game_IMG_heart3),
         };
-        game_IMG_alien = findViewById(R.id.game_IMG_ufo1_1);
-        ll = findViewById(R.id.game_LIST_lane1);
-    };
-
-    public void goRight() {
-        if(manager.getSpaceshipLocation()!=manager.getMAXRIGHT()) {
-            game_IMG_spaceships[manager.getSpaceshipLocation()].setVisibility(View.INVISIBLE);
-            game_IMG_spaceships[manager.getSpaceshipLocation()+1].setVisibility(View.VISIBLE);
-            manager.setSpaceship_location(1);
-        }
+        game_IMG_UFO = new ShapeableImageView[]{
+                findViewById(R.id.game_IMG_ufo1),
+                findViewById(R.id.game_IMG_ufo2),
+                findViewById(R.id.game_IMG_ufo3),
+        };
+        game_LANE_lane = new LinearLayout[]{
+                findViewById(R.id.game_LIST_lane1),
+                findViewById(R.id.game_LIST_lane2),
+                findViewById(R.id.game_LIST_lane3)
+        };
+        game_ET_toast = findViewById(R.id.game_ET_toast);
     }
 
-    public void goLeft() {
-        if(manager.getSpaceshipLocation()!=manager.getMAXLEFT()) {
-            game_IMG_spaceships[manager.getSpaceshipLocation()].setVisibility(View.INVISIBLE);
-            game_IMG_spaceships[manager.getSpaceshipLocation()-1].setVisibility(View.VISIBLE);
-            manager.setSpaceship_location(-1);
-        }
-    }
+
 
 }
--- app/src/main/java/com/example/game3lanes/gameManager.java
+++ app/src/main/java/com/example/game3lanes/gameManager.java
@@ -1,32 +1,107 @@
 package com.example.game3lanes;
 
 
-import android.content.Context;
-
-import com.google.android.material.imageview.ShapeableImageView;
-
 public class gameManager {
-    private long score = 0;
-    private int spaceship_location = 1;
-    private int obstacle_X;
-    private int obstacle_Y;
-    public final int MAXRIGHT = 2;
-    public final int MAXLEFT = 0;
-    public final int BONUS = 10;
-    public final int DELAY = 1000;
+    private final int LANES = 3;
+    private final int MAXRIGHT = LANES - 1;
+    private final int MAXLEFT = 0;
+    private final int SHIPHEIGHT = 50;
+    private final int BONUS = 10;
+    private final int MINVOL = 8;
+    private final int DELAY = 50;
+    private final int MAXLIFE = 3;
 
-    public static boolean check_collision() {
+    private int life = 3;
+    private long score = 0;
+    private int bonusScore = 0;
+
+    private int spaceship_location = 1;
+    private int[] UFO_locationY;
+    private int[] movementSpeeds;
+    private int laneLength;
+
+
+    private boolean[] hasUFO;
+
+    public gameManager(){
+        UFO_locationY = new int[LANES];
+        movementSpeeds = new int[LANES];
+        hasUFO = new boolean[]{
+                false,
+                false,
+                false
+        };
+    }
+
+    public void createUFO(){
+        for (int i=0; i<LANES; i++){
+            if(!getHasAsteroid(i)) {
+                int num = (int) (((Math.random()) * 100) + 1);
+                if (num > 50) {
+                    setHasUFO(i, true);
+                    generateMovSpeed(i);
+                }
+            }
+        }
+
+    }
+
+    public void moveUFO(){
+        for (int i = 0; i<LANES ;i++){
+            if(getHasAsteroid(i) && UFO_locationY[i] <= laneLength)
+                UFO_locationY[i] += movementSpeeds[i];
+        }
+    }
+
+    public boolean check_collision(int i) {
+            if(getHasAsteroid(i)) {
+                if ((get_UFO_locationY(i) + DELAY + getSHIPHEIGHT() >= (getLaneLength())) && (getUFOlocationX(i) == getSpaceshipLocation())) {
+                    return true;
+                }
+            }
         return false;
+    }
+
+    public boolean gotToEnd(int i){
+        if(getHasAsteroid(i)) {
+            if (get_UFO_locationY(i) + getSHIPHEIGHT() >= getLaneLength()-50)
+                return true;
+        }
+        return false;
+    }
+
+
+    public void setBonusScore(int replacer){
+        if(replacer<0 && (bonusScore + replacer) < 0)
+            this.bonusScore = 0;
+        else
+            bonusScore = bonusScore + replacer;
+    }
+
+    public void generateMovSpeed(int i){
+            int vol = (int) (((Math.random()) * MINVOL) + MINVOL);
+            movementSpeeds[i] = vol;
+    }
+
+    public int getMovementSpeed(int i){
+        return movementSpeeds[i];
     }
 
     public long getScore() {
         return score;
     }
 
+    public int getDELAY() {
+        return DELAY;
+    }
+
     public void setScore(long score) {
         this.score = score;
     }
 
+    public void addScore(long score) {
+        this.score += score;
+    }
     public int getSpaceshipLocation() {
         return spaceship_location;
     }
@@ -35,21 +110,6 @@ public class gameManager {
         spaceship_location += adder;
     }
 
-    public int getObstacle_X() {
-        return obstacle_X;
-    }
-
-    public void setObstacle_X(int obstacle_X) {
-        this.obstacle_X = obstacle_X;
-    }
-
-    public int getObstacle_Y() {
-        return obstacle_Y;
-    }
-
-    public void setObstacle_Y(int obstacle_Y) {
-        this.obstacle_Y = obstacle_Y;
-    }
 
     public int getMAXRIGHT() {
         return MAXRIGHT;
@@ -59,5 +119,56 @@ public class gameManager {
         return MAXLEFT;
     }
 
+    public int getLANES() {
+        return LANES;
+    }
 
+    public int getUFOlocationX(int i) {
+        return i;
+    }
+
+    public int get_UFO_locationY(int i) {
+        return UFO_locationY[i];
+    }
+
+    public void setUFO_locationY(int i, int adder) {
+        this.UFO_locationY[i] += adder;
+    }
+
+    public int getLaneLength() {
+        return laneLength;
+    }
+
+    public void setLaneLength(int laneLength) {
+        this.laneLength = laneLength;
+    }
+
+    public boolean getHasAsteroid(int i) {
+        return hasUFO[i];
+    }
+
+    public void setHasUFO(int i, boolean bool) {
+        hasUFO[i] = bool;
+    }
+
+    public int getLife() {
+        return life;
+    }
+
+    public void setLife(int life) {
+        this.life = life;
+    }
+
+    public int getMAXLIFE() {
+        return MAXLIFE;
+    }
+
+    public int getBonusScore(){
+        return bonusScore;
+    }
+
+
+    public int getSHIPHEIGHT() {
+        return SHIPHEIGHT;
+    }
 }
